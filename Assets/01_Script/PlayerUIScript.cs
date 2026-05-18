71961dea7cb1ef221ce528ac4558d7c3982e7b62
귀여운_크리쳐 <73733941+osch7497@users.noreply.github.com>
2026-05-18 17:58:28 +0900

    reuopdte
    reuopdte

--- Assets/01_Script/PlayerUIScript.cs
+++ Assets/01_Script/PlayerUIScript.cs
@@ -5,19 +5,28 @@ public class PlayerUIScript : MonoBehaviour
 {
     private GameObject FieldUI;
     private GameObject ItemIndicateUI;
-    public PlayerScript playerscript; 
+    private GameObject Minimap;
+    public PlayerScript playerscript;
+    public Transform ResourcesFolder;
     void Start()
     {
         FieldUI = transform.GetChild(0).gameObject;
         ItemIndicateUI = transform.GetChild(1).gameObject;
+        Minimap = transform.GetChild(2).gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //--item indicator 
         for(int i = 0; i < 3; i++) {
             Debug.Log($"{i} refresh");
             ItemIndicateUI.transform.GetChild(i).GetChild(1).GetComponent<Text>().text = $"{playerscript.BagPackResource[i]}";
         }
+        //--minimap
+        for (int i = 0; i < ResourcesFolder.childCount; i++) {
+            
+        }
+
     }
 }
