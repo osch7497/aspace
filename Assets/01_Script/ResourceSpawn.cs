7d039ecbbca9c4b99d2bb86cb634aa47b67ab349
귀여운_크리쳐 <73733941+osch7497@users.noreply.github.com>
2026-05-29 16:45:06 +0900

    test
    test

--- Assets/01_Script/ResourceSpawn.cs
+++ Assets/01_Script/ResourceSpawn.cs
@@ -2,15 +2,6 @@ using UnityEngine;
 
 public class ResourceSpawn : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Header("ResourceSpawn Min - Max")]
+    public Vector2 SpawnRadius;
 }
